a024587c2127e50aa6eb59fd417cb68fe72832fb
Muhammad Khan <muhammadkh4n@gmail.com>
2017-07-31 21:09:27 +0500

    Added register form, validations

--- auth-mean-client/src/app/app.module.ts
+++ auth-mean-client/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 
+// Components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
@@ -11,6 +12,9 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
+
+// Services
+import { ValidateService } from './services/validate.service';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
@@ -36,7 +40,7 @@ const appRoutes: Routes = [
     HttpModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [ValidateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- auth-mean-client/src/app/components/register/register.component.ts
+++ auth-mean-client/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  name: String
+  username: String
+  email: String
+  password: String
+  // confirmPassword: String
 
-  constructor() { }
+  constructor(private validateService: ValidateService) { }
 
   ngOnInit() {
+  }
+
+  registerUser() {
+    const user = {
+      name: this.name,
+      email: this.email,
+      username: this.username,
+      password: this.password
+    };
+
+    // Required Fields
+    if (!this.validateService.validateRegister(user)) {
+      console.log('Fill in all fields');
+      return false;
+    }
+
+    // Email Validate
+    if (!this.validateService.validateEmail(user.email)) {
+      console.log('Use a valid email');
+      return false;
+    }
   }
 
 }
