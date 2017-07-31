6df5b7ffcbcda73f8f939cd5c2033f4d3d0506b7
Muhammad Khan <muhammadkh4n@gmail.com>
2017-07-31 18:56:47 +0500

    some fixes, auth test

--- routes/users.js
+++ routes/users.js
@@ -76,8 +76,12 @@ router.post('/authenticate', (req, res, next) => {
 });
 
 // Profile
-router.get('/profile', (req, res, next) => {
-  res.send('PROFILE');
+router.get('/profile', 
+           passport.authenticate('jwt', {session:false}), 
+           (req, res, next) => {
+  res.json({
+    user: req.user
+  });
 });
 
 module.exports = router;
